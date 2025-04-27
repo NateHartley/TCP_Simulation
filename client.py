3e2b75ceb68557a57c109ad34901fe4d529c43a6
NateHartley <nathanhartley82@gmail.com>
2025-04-27 15:19:39 +0100

    TCP handshake flags can be sent

--- client.py
+++ client.py
@@ -1,23 +1,45 @@
 import socket
+import sys
 
 client_conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# same as server_conn
-
 client_conn.connect(("localhost", 23456))
-# must be server hostname or ip and port
-# using connect() rather than bind()
 
 while True:
+    print("\nSelect an option: \n[1] - Establish TCP connection \n[2] - Send data")
 
-    usr_input = input("Speak to server: ")
-    encoded_input = usr_input.encode("utf-8")
+    try:
+        usr_input = input()
 
-    client_conn.sendall(encoded_input)
-    # sending data
+        if usr_input == "1":
+            # Send
+            print("\n[1] SYN: Client -> Server")
+            client_conn.send(bytes("SYN", "utf-8"))
+            print("Client waiting for SYN-ACK...")
 
-    data = client_conn.recv(1024)
-    # hopefully get same data back from server
+            # Receive
+            data = client_conn.recv(1024)
+            decoded_data = data.decode("utf-8")
 
-    decoded_data = data.decode("utf-8")
+            if decoded_data == "SYN-ACK":
+                print("\n3] SYN: Client -> Server")
+                client_conn.send(bytes("ACK", "utf-8"))
 
-    print(f"Data received: {decoded_data}")
+                
+            # need to add in sequence numbers here or something and add if statements for those otherwise i dont think things are gonna work
+
+            print(f"Data received from server: {decoded_data}")
+        elif usr_input == "2":
+            print("Send message to server")
+            while True:
+                message = input()
+                client_conn.send(bytes(message, "utf-8"))
+        else:
+            print("\nERROR - Invalid selection")
+
+    except KeyboardInterrupt:
+        print("\nERROR - Keyboard Interrupt")
+        # dont need this, only in server
+        # client_conn.shutdown(socket.SHUT_RDWR)
+        # client_conn.close()
+        sys.exit(1)
+        break
--- server.py
+++ server.py
@@ -1,33 +1,46 @@
 import socket
+import sys
 
 server_conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# AF_INET is the Address Family (AF) for the socket, in this case it's ipv4 (INET)
-# TODO: eventually I want to replace SOCK_STREAM with SOCK_RAW as this just sends the raw packet and it's my job to do the encapsulation
-
-server_conn.bind(("", 23456))
-# bind() takes in a tuple and is used to associate a socket with a specific ip and port
-# 127.0.0.1 just a standard loopback interface address (localhost)
-# 23456 port number that hopefully isn't being used by any other service
-
+server_conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Server can be re-issued immediately
+server_conn.bind(("127.0.0.1", 23456))
 server_conn.listen()
-# listen() takes an integer that is the number of unaccepted connections that the system will allow before refusing new connections
-
 conn, addr = server_conn.accept()
-# accept() blocks execution and waits for incoming connection. Once it receives a socket object, it assigns it to the connection to conn and host+port to addr
-# this is the socket used to talk to the client
 
 with conn:
-    # with automatically closes the connection once we're done so no need to specify closing the connection
-    
     print(f'Connection initiated with {addr}')
 
-    # infinite while loop, so the server is always going to echo back whatever the client sends
     while True:
-        data = conn.recv(1024)
-        # recv() is a blocking method, it blocks everything else whilst it waits for data from the Standard Input (I/O)
-        # its waiting to receive data from the client
-        # recv() takes in a buffer size which we've set to be 1024 bytes (?)
+        try:
+            # Receive
+            data = conn.recv(1024)
+            data_dec = data.decode("utf-8")
 
-        if not data: break
-        conn.sendall(data)
-        # echo the data it just received
+            print(f"Server received data: {data_dec}")
+
+            
+
+            # breaks loop if data is empty
+            if not data: 
+                print("Client disconnected...")
+                break
+
+            # Send
+
+            if data_dec == "SYN":
+                print("\n[2] SYN: Server -> Client")
+                conn.send(bytes("SYN-ACK", "utf-8"))
+                print("Server waiting for ACK...")
+            if data_dec == "ACK":
+                print("\nTCP Connection initiated!")
+
+            # print("Sending this back to the client..")
+            # conn.sendall(data)
+
+        except KeyboardInterrupt:
+            print("\nERROR - Keyboard Interrupt")
+            conn.shutdown(socket.SHUT_RDWR) # needs to be conn not server_conn
+            conn.close()
+            sys.exit(1)
+            break
+
