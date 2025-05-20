47ef659bb3b2938f49915ca8b2ebea7bc78a1105
NateHartley <nathanhartley82@gmail.com>
2025-05-20 21:46:47 +0100

    Utilises packet object to transmit combined payload of message and flags between client and server. States are also introduced

--- client.py
+++ client.py
@@ -1,48 +1,57 @@
 import socket
 import sys
 import json
+from packet import Packet
 
 client_conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_conn.connect(("localhost", 23456))
 
-C_FLAGS = {"CWR":0, "ECE":0, "URG":0, "ACK":0, "PSH":0, "RST":0, "SYN":0, "FIN":0}
+C_STATES = ["CLOSED", "SYN-SENT", "ESTABLISHED", "FIN-WAIT-1", "FIN-WAIT-2", "TIME-WAIT"]
+state = C_STATES[0]
 TCP_conn = False
+c_packet = Packet()
 
 while True:
     try:
-        print("\nSelect an option: \n(1) - Establish TCP connection \n(2) - Send data")
+        print("\nSelect an option: \n(1) - Establish TCP connection \n(2) - Send data \n(3) - Exit client")
         usr_input = input()
 
         if usr_input == "1":
-            if TCP_conn == True:
-                print("\nTCP connection has already been established.")
-            else:
-                print("\n--------- Establishing TCP connection ---------")
+            if not TCP_conn:
+                state = C_STATES[1]
 
-                # Send
+                # Send SYN to server
+                print("\n--------- Establishing TCP connection ---------")
                 print("[1] SYN: Client -> Server")
-                C_FLAGS.update({"SYN":1})
-                flags_json = json.dumps(C_FLAGS) # Serialise data, turn dictionary into string
-                client_conn.send(bytes(flags_json, "utf-8"))
-                C_FLAGS.update({"SYN":0})
+                c_packet.set_flag("SYN")
+                p = json.dumps(c_packet.to_dict()) # Serialise data, turn dictionary into string
+                client_conn.send(bytes(p, "utf-8"))
+                c_packet.clear_flag("SYN")
                 print("Client waiting for SYN-ACK...")
 
-                # Receive
-                d = client_conn.recv(1024)
-                data = d.decode("utf-8")
-                server_flags = json.loads(data)
+                # Server data received
+                data = client_conn.recv(1024).decode("utf-8")
 
-                if server_flags["SYN"] == 1 and server_flags["ACK"] == 1:
+                # Tries to deserialise the data
+                try:
+                    s_packet = json.loads(data)
+                except:
+                    print("ERROR: Data can't be deserialised.")
+
+                # Send ACK to establish connection with server
+                if s_packet["flags"]["SYN"] == 1 and s_packet["flags"]["ACK"] == 1:
+                    state = C_STATES[2]
                     print(f"\n[3] ACK: Client -> Server")
-                    C_FLAGS.update({"ACK":1})
-                    flags_json = json.dumps(C_FLAGS)
-                    client_conn.send(bytes(flags_json, "utf-8"))
-                    C_FLAGS.update({"ACK":0})
+                    c_packet.set_flag("ACK")
+                    p = json.dumps(c_packet.to_dict())
+                    client_conn.send(bytes(p, "utf-8"))
+                    c_packet.clear_flag("ACK")
                     print("\nTCP Connection Established! 🎉")
-                    TCP_conn = True
                     print("-----------------------------------------------\n")
-
-            print(f"DEBUG: Data received from server: {data}")
+                    TCP_conn = True
+            
+            else:
+                print("\nTCP connection has already been established.")
         
         elif usr_input == "2":
             if TCP_conn == True:
@@ -52,9 +61,15 @@ while True:
                     if message == "EXIT":
                         print("Exiting...")
                         break
-                    client_conn.send(bytes(message, "utf-8"))
+                    c_packet.set_flag("ACK")
+                    c_packet.data = message
+                    p = json.dumps(c_packet.to_dict())
+                    client_conn.send(bytes(p, "utf-8"))
             else:
                 print("\nA TCP connection needs to be established before messages can be transmitted.")
+        elif usr_input == "3":
+            print("Closing connection...")
+            break
         else:
             print("\nERROR - Invalid selection")
 
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 import json
+from packet import Packet
 
 server_conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Server can be re-issued immediately
@@ -8,46 +9,53 @@ server_conn.bind(("127.0.0.1", 23456))
 server_conn.listen()
 conn, addr = server_conn.accept()
 
-S_FLAGS = {"CWR":0, "ECE":0, "URG":0, "ACK":0, "PSH":0, "RST":0, "SYN":0, "FIN":0}
+S_STATES = ["CLOSED", "LISTEN", "SYN-RECEIVED", "ESTABLISHED", "CLOSE-WAIT", "LAST-ACK"]
+state = S_STATES[0]
+s_packet = Packet()
 
 with conn:
     print(f'Connection initiated with {addr}')
+    state = S_STATES[1]
 
     while True:
         try:
-            # Receive
-            d = conn.recv(1024)
-            data = d.decode("utf-8")
+            # Client data received
+            data = conn.recv(1024).decode("utf-8")
 
+            # Tries to deserialise the data
             try:
-                client_flags = json.loads(data)
+                c_packet = json.loads(data)
             except:
-                print("Data can't be deserialised.")
+                print("ERROR: Data can't be deserialised.")
 
-            # breaks loop if data is empty
+            # Breaks loop if data is empty
             if not data: 
                 print("Client disconnected...")
                 break
 
-            # Send
-            if client_flags["SYN"] == 1:
+            # Send SYN-ACK to client
+            if c_packet["flags"]["SYN"] == 1:
+                state = S_STATES[2]
                 print("\n--------- Establishing TCP connection ---------")
-
                 print("[2] SYN-ACK: Server -> Client")
-                S_FLAGS.update({"SYN":1})
-                S_FLAGS.update({"ACK":1})
-                flags_json = json.dumps(S_FLAGS)
+                s_packet.set_flag("SYN")
+                s_packet.set_flag("ACK")
+                flags_json = json.dumps(s_packet.to_dict())
                 conn.send(bytes(flags_json, "utf-8"))
-                S_FLAGS.update({"SYN":0})
-                S_FLAGS.update({"ACK":0})
+                s_packet.clear_flag("SYN")
+                s_packet.clear_flag("ACK")
                 print("Server waiting for ACK...")
-                client_flags["SYN"] = 0 # reset it otherwise it will keep triggering this when client sending messages bc this var doesn't get nulled
-            if client_flags["ACK"] == 1:
+
+            # Send ACK to establish connection with client
+            if c_packet["flags"]["ACK"] == 1 and state == S_STATES[2]:
+                state = S_STATES[3]
                 print("\nTCP Connection Established! 🎉")
                 print("-----------------------------------------------\n")
-                client_flags["ACK"] = 0
-
-            print(f"DEBUG: Data received from client: {data}")
+            
+            # Server in established state, waiting for data from client
+            if c_packet["flags"]["ACK"] == 1 and state == S_STATES[3]:
+                if c_packet["data"] != "":
+                    print("Received from client: ", c_packet["data"])
 
         except KeyboardInterrupt:
             print("\nERROR - Keyboard Interrupt")
